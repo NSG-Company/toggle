c319960bbeedb1b735939f2fd1570df644a83b1d
Newien Gopie <33331871+NSG-Company@users.noreply.github.com>
2021-05-17 11:10:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const attachEventToAlertButton = function () {
+    const alertButton = document.getElementById("alert-button");
+    alertButton.addEventListener("click", function () {
+        alert("hello World!");
+    });
+
+};
+
+
+
+const toggleColor = function () {
+    const bodyElement = document.body;
+    bodyElement.classList.toggle("red-background");
+};
+
+
+const attachEventToChangeColorButton = function () {
+    const changeColorButton = document.getElementById("change-background-button");
+    changeColorButton.addEventListener("click", function () {
+        toggleColor();
+    });
+};
+
+
+attachEventToAlertButton();
+attachEventToChangeColorButton();
+
+
+
